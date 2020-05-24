63206556540da8a764b49860b34128f77152fd2c
Esdras <esdraspiano@gmail.com>
2020-05-24 02:23:25 -0300

    Leitura de CSV e inserção no banco

--- apps/core/views.py
+++ apps/core/views.py
@@ -14,9 +14,65 @@ from .models import Tendencias
 from .models import Equipe
 from .models import Noticias
 
+import csv 
+from django.conf import settings
+from pprint import pprint
+from datetime import datetime
+from apps.core.models.boletimEpidemiologico import BoletimEpidemiologico
+
 def home(request):
 	url = "base.html"
 	context = Home.getContext(Home)
+
+	jatai = {"header": [], "data": []}
+	with open(settings.BASE_DIR + '/dados/jatai-dados-20-05-22.csv') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		line_count = 0
+		for row in csv_reader:
+			if line_count == 0:
+				i = 0
+				for header in row:
+					jatai["header"].append(header)
+					i += 1
+				line_count += 1
+			else:
+				i = 0
+				data = []
+				for value in row:
+					if(value == '' or value == '-'):
+						data.append(0)
+					else:
+						data.append(value)
+					i += 1
+				jatai["data"].append(data)
+				line_count += 1
+
+		for row in jatai["data"]:
+			str_date = row[0]
+			date = datetime.strptime(str_date, '%d/%m/%Y').date()
+			row[0] =  date
+
+			registro = {
+				"cidade": "jatai",
+				"data_atualizacao": row[0],
+				"fonte_oficial": "http://www.google.com",
+				"confirmados": row[1],
+				"recuperados": row[8],
+				"obitos": row[9],
+				"suspeitos": 0,
+				"investigados":  row[3],
+				"descartados": row[2],
+				"monitorados": row[7],
+				"notificados": row[4],
+				"isolados": row[5],
+				"internados": row[6],
+				"enfermaria": None,
+				"uti": None
+			}
+
+			insercao = BoletimEpidemiologico(**registro)
+			insercao.save()
+		
 
 	return render(request, url, context)
 
